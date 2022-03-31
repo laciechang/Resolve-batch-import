25b8d250fe981af366296a03e7218d149429486a
laciechang <laciechang@163.com>
2022-04-01 00:50:26 +0800

    first upload

---
+++ Batch_Importer.py
@@ -0,0 +1,129 @@
+import glob
+import time
+
+try:
+    import importlib.util
+    
+    def load_dynamics(module, path):
+        spec = importlib.util.spec_from_file_location(module, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        return mod
+    def load_dynamic(module, path):
+        loader = importlib.machinery.ExtensionFileLoader(module, path)
+        module = loader.load_module()
+        return module
+    
+except ImportError:
+    import imp
+    # py2
+    def load_dynamic(module, path):
+        spec = imp.load_dynamic(module, path)
+        return spec
+
+def getresolve(app='Resolve'):
+    pylib = "/Applications/DaVinci Resolve/DaVinci Resolve.app/Contents/Libraries/Fusion/fusionscript.so"
+    dvr_script = load_dynamic('fusionscript', pylib)
+    return dvr_script
+
+bmd = getresolve()
+
+fu = bmd.scriptapp('Fusion')
+resolve = bmd.scriptapp('Resolve')
+pj = resolve.GetProjectManager().GetCurrentProject()
+
+ui = fu.UIManager
+disp = bmd.UIDispatcher(ui)
+
+ProgressBar = "ProgressBar"
+ProgressBarBG = "ProgressBarBG"
+input_pattern = "input_pattern"
+add_path = "add_path"
+clean_path = "clean_path"
+start_import = "start_import"
+infomation = "infomation"
+path_tree = "path_tree"
+
+window_01 = [
+        ui.VGroup({"Spacing": 5},
+        [
+            ui.HGroup({"Spacing": 5, "Weight": 0},
+            [
+                ui.LineEdit({"ID": input_pattern, "PlaceholderText": "e.g. /Volumes/*/Source"}),
+                ui.Button({"ID": add_path, "Text": "添加路径", "Weight": 0}),
+            ]),
+            ui.Tree({"ID": path_tree, "AlternatingRowColors": True, "HeaderHidden": True, "SelectionMode": "ExtendedSelection", "Weight": 2}),
+            ui.HGroup({"Spacing": 5, "Weight": 0},
+            [
+                ui.Button({"ID": clean_path, "Text": "清除所有", "Weight": 0}),
+                ui.Label({"ID": infomation, "Text": "" , "Weight": 1}),
+                ui.Button({"ID": start_import, "Text": "导入选中部分", "Weight": 0}),
+            ]),
+            ui.Stack({"ID": "pg_set", "Weight": 0},[
+                        ui.Label({"ID": ProgressBarBG, "Visible":False, "StyleSheet": "max-height: 3px; background-color: rgb(37,37,37)",}),
+                        ui.Label({"ID": ProgressBar, "Visible":False, "StyleSheet": "max-height: 1px; background-color: rgb(102, 221, 39);border-width: 1px;border-style: solid;border-color: rgb(37,37,37);",})
+            ]),
+        ])
+]
+
+dlg = disp.AddWindow({ 
+                        "WindowTitle": "批量导入媒体池", 
+                        "ID": "MyWin", 
+                        "Geometry": [ 
+                                    600, 300, # position when starting
+                                    800, 300 #width, height
+                         ], 
+                        }, window_01)
+
+itm = dlg.GetItems()
+
+def increase_pgbar(ratio):
+    itm[ProgressBar].Visible = True
+    total_width = int(itm[ProgressBarBG].GetGeometry()[3])
+    width = total_width * ratio
+    itm[ProgressBar].Resize([int(width), 3])
+
+itm[ProgressBar].Resize([1, 3])
+
+def _show_all_path(ev):
+    file_list = glob.glob(itm[input_pattern].Text)
+    toplevelitems = []
+    for i in file_list:
+        row = itm[path_tree].NewItem()
+        row.Text[0] = i
+        toplevelitems.append(row)
+    itm[path_tree].AddTopLevelItems(toplevelitems)
+
+def _clean_all_path(ev):
+    itm[path_tree].Clear()
+
+def _start_import(ev):
+    itm[start_import].Enabled = False
+    file_list = []
+    items = itm[path_tree].SelectedItems()
+    for i in items:
+        file_list.append(items[i].Text[0])
+        
+    length = len(file_list)
+    ms = resolve.GetMediaStorage()
+    for i in range(0, length):
+        itm[infomation].Text = " 🔍 " + str(file_list[i])
+        ratio = float(i/(length-1)) if length>1 else 1
+        increase_pgbar(ratio)
+        ms.AddItemListToMediaPool(str(file_list[i]))
+        time.sleep(0.2)
+    itm[ProgressBar].Visible = False
+    itm[start_import].Enabled = True 
+
+def _func(ev):
+    disp.ExitLoop()
+
+dlg.On.MyWin.Close = _func
+dlg.On[start_import].Clicked = _start_import
+dlg.On[add_path].Clicked = _show_all_path
+dlg.On[clean_path].Clicked = _clean_all_path
+
+if __name__ == "__main__":
+    dlg.Show()
+    disp.RunLoop()
+    dlg.Hide()
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Resolve-batch-import
 同时从多个位置中导入媒体至媒体池
+
+# 🔧 安装
+
+- 请将 *Batch_Importer.py* 拷贝至达芬奇指定的脚本存放目录下
+
+- macOS: /Users/{你的用户名}/Library/Application Support/Blackmagic Design/DaVinci Resolve/Fusion/Scripts/Edit
+
+- Windows: C:\Users\{你的用户名}\AppData\Roaming\Blackmagic Design\DaVinci Resolve\Support\Fusion\Scripts\Edit
+
+- 使用 v17.3 及以上版本
+
+- 在菜单：工作区(Workspace) > 脚本(Scripts) 中即可找到
+
+- 仅支持在达芬奇内使用，不支持在外部运行
+
+  
+
+# 🎛 用法
+
+- 在地址栏中输入路径，并配合“星号 *”进行模糊匹配，例如 /Documents/A* 即表示Documents路径下，以Test开头的多个文件或文件夹
+
+- 点击【添加路径】即可讲匹配到的所有路径添加到下方待定区域，可以看到匹配的结果
+
+- 匹配有误则可以【清除所有】
+
+- 在待定列表中选择所需的部分路径，或者直接全选，点击【导入选中部分】即可开始执行导入（到当前媒体夹）
+
+
+
+# 🧷 TIPS 
+
+一次性导入多块素材盘的素材
+
+将位于多个不同位置、符合特定规则的文件夹、文件导入媒体池中
+
+
+# ☝️ 需要
+
+- Python 3.6 64-bit 
+- 其他版本的 Python 达芬奇并不支持
